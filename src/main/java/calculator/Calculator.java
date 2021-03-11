ca0e5ac44983612c18cbe8b9c1fc0360a49c9508
Rahul-15 <31342612+Rahul-15@users.noreply.github.com>
2021-03-11 18:23:06 +0530

    Update Calculator.java

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -7,7 +7,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 public class Calculator {
 
-    private static final Logger logger = LogManager.getLogger(Calculator.class);
+    //private static final Logger logger = LogManager.getLogger(Calculator.class);
     public Calculator() {
     }
 
@@ -70,31 +70,31 @@ public class Calculator {
 
 
     public double factoral(double number1) {
-        logger.info("[FACTORIAL] - " + number1);
+       // logger.info("[FACTORIAL] - " + number1);
         double result = fact(number1);
-        logger.info("[RESULT - FACTORIAL] - " + result);
+       // logger.info("[RESULT - FACTORIAL] - " + result);
         return result;
     }
 
 
 
     public double sqroot(double number1) {
-        logger.info("[SQ ROOT] - " + number1);
+        //logger.info("[SQ ROOT] - " + number1);
         double result = Math.sqrt(number1);
-        logger.info("[RESULT - SQ ROOT] - " + result);
+        //logger.info("[RESULT - SQ ROOT] - " + result);
         return result;
     }
 
 
     public double power(double number1, double number2) {
-        logger.info("[POWER] - " + number1 + "RAISED TO " + number2);
+       // logger.info("[POWER] - " + number1 + "RAISED TO " + number2);
         double result = Math.pow(number1,number2);
-        logger.info("[RESULT - POWER] - " + result);
+       // logger.info("[RESULT - POWER] - " + result);
         return result;
     }
 
     public double naturalLog(double number1) {
-        logger.info("[NATURAL LOG] - " + number1);
+        //logger.info("[NATURAL LOG] - " + number1);
         double result = 0;
         try {
 
@@ -108,7 +108,7 @@ public class Calculator {
         } catch (ArithmeticException error) {
             System.out.println("[EXCEPTION - LOG] - Cannot find log of negative numbers " + error.getLocalizedMessage());
         }
-        logger.info("[RESULT - NATURAL LOG] - " + result);
+        //logger.info("[RESULT - NATURAL LOG] - " + result);
         return result;
     }
     public double fact(double num) {
